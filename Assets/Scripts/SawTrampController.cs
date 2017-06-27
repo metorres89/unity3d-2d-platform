ca45d2a89f5d5e030877c0eaf5b17ce45dad4f23
Marcos E. Torres <metorres89@gmail.com>
2017-06-26 21:49:04 -0300

    Se agregó controlador a la trampa saw

---
+++ Assets/Scripts/SawTrampController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawTrampController : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.GetComponent<PlayerController> ().ReceiveDamage (1.0f);
+		} else if (col.gameObject.tag == "Enemy") {
+			col.gameObject.GetComponent<ZombieController> ().ReceiveDamage (1.0f);
+		}
+	}
+}
