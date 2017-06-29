006cf51e70b31635786a7b16025ae54c30a8abc0
Marcos E. Torres <metorres89@gmail.com>
2017-06-29 00:45:03 -0300

    Se reorganizó la carpeta de assets.
    Se agregó un prefab para la puerta de salida, y switches.
    La puerta sólo se abre cuando todos los switches pasaron a estar activos (color verde).
    
    Se agregó sonido fx para la alteración de los switches y de la puerta.

---
+++ Assets/Scripts/ExitDoorController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoorController : MonoBehaviour {
+
+	private Animator myAnimator;
+	private DoorState currentState;
+	private bool axisInUse = false;
+
+	public float delayOpenDoor = 1.0f;
+
+	public enum DoorState
+	{
+		LOCKED = 0,
+		UNLOCKED = 1,
+		OPEN = 2
+	};
+
+	public string switchTag = "Switch";
+	public string responsableAxis = "Fire2";
+
+	void Awake() {
+		currentState = DoorState.LOCKED;
+		axisInUse = false;
+	}
+
+	void Start () {
+		myAnimator = gameObject.GetComponent<Animator> ();
+	}
+
+	void Update() {
+		if (Input.GetAxisRaw (responsableAxis) == 0.0f) {
+			axisInUse = false;
+		}
+
+		if (currentState == DoorState.UNLOCKED) {
+
+			delayOpenDoor -= Time.deltaTime;
+
+			if (delayOpenDoor <= 0.0f) {
+
+				setState (DoorState.OPEN);
+
+			}
+		}
+	}
+
+	private void setState(DoorState newState){
+		if (newState != currentState) {
+			currentState = newState;
+
+			myAnimator.SetInteger ("state", (int)currentState);
+		}
+	}
+
+	public DoorState getState() {
+		return currentState;
+	}
+
+	void OnTriggerStay2D(Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			
+			if (Input.GetAxis ("Fire2") != 0.0f && axisInUse == false && currentState == DoorState.LOCKED) {
+
+				axisInUse = true;
+
+				bool tryToUnlock = TryToUnlock ();
+
+				Debug.LogFormat ("Player is trying to unlock the door , the result: {0}", tryToUnlock);
+
+				if (tryToUnlock) {
+					setState (DoorState.UNLOCKED);
+
+					FXAudio.playClip ("DoorUnlocked", 0.5f);
+				
+				} else {
+					
+					FXAudio.playClip ("DoorLocked", 0.5f);
+				}
+			}
+		}
+	}
+
+	public bool TryToUnlock () {
+		GameObject[] allSwitches = GameObject.FindGameObjectsWithTag (switchTag);
+
+		int greenSwitches = 0;
+
+		foreach (GameObject goSwitch in allSwitches) {
+
+			ExitDoorSwitchController edsc = goSwitch.GetComponent<ExitDoorSwitchController> ();
+			if (edsc.getState ())
+				greenSwitches++;
+		}
+
+		return greenSwitches == allSwitches.Length;
+	}
+}
---
+++ Assets/Scripts/ExitDoorSwitchController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoorSwitchController : MonoBehaviour {
+
+	private Animator myAnimator;
+	private bool currentState;
+	private bool axisInUse = false;
+	public string responsableAxis = "Fire2";
+
+	void Awake() {
+		currentState = false;
+	}
+	// Use this for initialization
+	void Start () {
+		myAnimator = gameObject.GetComponent<Animator> ();	
+	}
+	
+	public void setState(bool newState) {
+
+		if (newState != currentState) {
+			currentState = newState;
+			myAnimator.SetBool ("state", currentState);
+		}
+
+	}
+
+	public bool getState() {
+		return currentState;
+	}
+
+	public void Update() {
+		if (Input.GetAxis (responsableAxis) == 0.0f) {
+			axisInUse = false;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+
+			if (Input.GetAxisRaw (responsableAxis) != 0.0f && axisInUse == false) {
+				axisInUse = true;
+				setState (!currentState);
+
+				if (currentState) {
+					FXAudio.playClip ("SwitchGreen", 0.5f);
+				} else {
+					FXAudio.playClip ("SwitchRed", 0.5f);
+				}
+			}
+
+		}
+	}
+}
