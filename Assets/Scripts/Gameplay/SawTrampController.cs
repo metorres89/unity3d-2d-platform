08993f30f824f535f5fa3fde2843bae5b7a6fbeb
Marcos E. Torres <metorres89@gmail.com>
2017-08-15 23:25:25 -0300

    Pequeños cambios en la pantalla de resultados.

--- Assets/Scripts/Gameplay/SawTrampController.cs
+++ Assets/Scripts/Gameplay/SawTrampController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SawTrampController : MonoBehaviour {
-
+	private Rigidbody2D myRigidBody;
 	public float damage = 1.0f;
-	public float impactForce = 200.0f;
+
+	void Start()
+	{
+		myRigidBody = gameObject.GetComponent<Rigidbody2D> ();
+
+		myRigidBody.angularVelocity = 360.0f;
+	}
 
 	void OnCollisionEnter2D(Collision2D col){
 
 		if (col.gameObject.tag == "Player") {
 			PlayerController pc = col.gameObject.GetComponent<PlayerController> ();
 			pc.ReceiveDamage (damage);
-			pc.ReceiveImpact (col.contacts[0].point * impactForce);
 		} else if (col.gameObject.tag == "Enemy") {
 			col.gameObject.GetComponent<ZombieController> ().ReceiveDamage (damage);
 
