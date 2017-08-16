d1b65d619cfcf935313dd4e5f1a795287ebeb537
Marcos E. Torres <metorres89@gmail.com>
2017-08-16 00:18:37 -0300

    arreglo de bug en escena de resultados.

--- Assets/Scripts/Result/ScorePanelController.cs
+++ Assets/Scripts/Result/ScorePanelController.cs
@@ -4,56 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScorePanelController : MonoBehaviour {
+
 	public Text gemScoreText;
 	public Text killScoreText;
 	public Text timeScoreText;
 	public Text totalScoreText;
 
-	// Use this for initialization
 	void Start () {
-		if (gemScoreText == null) {
-			Transform t = gameObject.transform.Find ("GemScoreLabel");
-			if (t != null)
-				gemScoreText = t.gameObject.GetComponent<Text> ();
-		}
-
-		if (killScoreText == null) {
-			Transform t = gameObject.transform.Find ("KillScoreLabel");
-			if (t != null)
-				killScoreText = t.gameObject.GetComponent<Text> ();
-		}
-
-		if (timeScoreText == null) {
-			Transform t = gameObject.transform.Find ("TimeScoreLabel");
-			if (t != null)
-				timeScoreText = t.gameObject.GetComponent<Text> ();
-		}
-
-		if (totalScoreText == null) {
-			Transform t = gameObject.transform.Find ("TotalScoreLabel");
-			if (t != null)
-				totalScoreText = t.gameObject.GetComponent<Text> ();
-		}
-
 		updateText ();
-
 	}
 
 	private void updateText() {
-		if (gemScoreText != null) {
-			gemScoreText.text = GameState.GetGemScore ().ToString();
-		}
-
-		if (killScoreText != null) {
-			killScoreText.text = GameState.GetKillScore ().ToString();
-		}
-
-		if (timeScoreText != null) {
-			timeScoreText.text = string.Format("minutes: {0} seconds: {1}", GameState.GetTimeMinuteScore (), GameState.GetTimeSecondScore());
-		}
-
-		if (totalScoreText != null) {
-			totalScoreText.text = GameState.GetTotalScore ().ToString ();
-		}
+		gemScoreText.text = GameState.GetGemScore ().ToString();
+		killScoreText.text = GameState.GetKillScore ().ToString();
+		timeScoreText.text = string.Format("minutes: {0} seconds: {1}", GameState.GetTimeMinuteScore (), GameState.GetTimeSecondScore());
+		totalScoreText.text = GameState.GetTotalScore ().ToString ();
 	}
 }
